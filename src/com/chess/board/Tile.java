5a4238278d3f42ba86a5a4281846b89a4da631ad
lordchingiss <jackli2014@gmail.com>
2020-08-26 18:30:07 -0700

    CE-12: Implemented Tile Class

--- src/com/chess/board/Tile.java
+++ src/com/chess/board/Tile.java
@@ -1,4 +1,37 @@
 package com.chess.board;
 
+import com.chess.pieces.Color;
+import com.chess.pieces.Piece;
+
 public class Tile {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Tile(int x, int y, Piece piece) {
+        this(x, y);
+        this.piece = piece;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        if (piece != null) {
+            String color = piece.getColor() == Color.WHITE ? "W" : "B";
+            return color + piece.toString();
+        }
+        return ".";
+    }
 }
